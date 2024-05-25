5824374804c30491a32c7d7d0ae5b5d1a94010c3
Royalgurjar123 <gurjarroyal647@gmail.com>
2024-05-25 11:22:18 +0530

    done

---
+++ newp10/src/main/java/com/rays/common/message/MessageDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.common.message;
+
+import com.rays.common.BaseDAOInt;
+
+/**
+ * Role DAO interface.
+ * Rahul gurjar 
+ */
+public interface MessageDAOInt extends BaseDAOInt<MessageDTO> {
+
+}
---
+++ newp10/src/main/java/com/rays/dao/CollegeDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.CollegeDTO;
+
+/**
+ * College DAO interface.
+ * Rahul gurjar 
+ */
+public interface CollegeDAOInt extends BaseDAOInt<CollegeDTO> {
+}
